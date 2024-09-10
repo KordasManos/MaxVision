037cce0ad7b0d4038ea7f44c9520702cc48965db
emanuel.kord@gmail.com <emanuel.kord@gmail.com>
2024-09-10 22:01:40 +0300

    About Section finished. Added Follow us section + new images

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ window.addEventListener('scroll', function () {
       navbar.classList.remove('navbar-scrolled');
   }
 });
+
+
+document.getElementById('videoThumbnail').addEventListener('click', function() {
+  var videoPopup = document.getElementById('videoPopup');
+  var videoFrame = document.getElementById('videoFrame');
+  var videoID = 'm2DMMMonc_0';
+
+  videoFrame.src = 'https://www.youtube.com/embed/' + videoID + '?autoplay=1';
+  videoPopup.style.display = 'flex';
+});
+
+document.getElementById('videoPopup').addEventListener('click', function(e) {
+  if (e.target !== this) return;
+
+  var videoPopup = document.getElementById('videoPopup');
+  var videoFrame = document.getElementById('videoFrame');
+
+  videoFrame.src = '';
+  videoPopup.style.display = 'none';
+});
